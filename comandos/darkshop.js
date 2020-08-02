2a175364e2f55b7f75c8d7a9575dcacb1e1eeb78
JeffreyRandom <35880312+JeffreyRandom@users.noreply.github.com>
2020-08-02 15:03:43 -0500

    Update darkshop.js

--- comandos/darkshop.js
+++ comandos/darkshop.js
@@ -215,7 +215,7 @@ module.exports.run = async (bot, message, args) => {
                                                     if (!Number.isInteger(c / itemPerPage))
                                                     totalpags++;
 
-                                                    let inicio = itemPerPage * pagn - itemPerPage;
+                                                    let inicio = itemPerPage * pagn - itemPerPage + 1;
                                                     let fin = itemPerPage * pagn;
 
                                                     inicio = inicio - 1;
