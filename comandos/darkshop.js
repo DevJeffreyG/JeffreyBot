2662352d98c4ff21443f778ddf6410f7c802a362
JeffreyRandom <35880312+JeffreyRandom@users.noreply.github.com>
2020-08-02 15:31:20 -0500

    Update darkshop.js

--- comandos/darkshop.js
+++ comandos/darkshop.js
@@ -224,7 +224,7 @@ module.exports.run = async (bot, message, args) => {
                                                     if (items.length < fin - 1) {
                                                     fin = items.length;
                                                     } else if (items.length === fin - 1) {
-                                                    fin = items.length - 1;
+                                                    fin = items.length + 1;
                                                     } else {
                                                     fin = fin - 1;
                                                     }
