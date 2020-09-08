931101393229a6722f98aca0bd7103910928375e
JeffreyRandom <35880312+JeffreyRandom@users.noreply.github.com>
2020-09-08 08:04:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 8080
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res) {
-  res.render("index");
+  res.redirect("..");
 })
 
 app.listen(port, function(){
