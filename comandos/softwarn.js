222b9dc73ba718bf08661f06f9621e2b02983f0a
JeffreyRandom <35880312+JeffreyRandom@users.noreply.github.com>
2020-09-24 18:42:37 -0500

    Update softwarn.js

--- comandos/softwarn.js
+++ comandos/softwarn.js
@@ -43,7 +43,6 @@ module.exports.run = async (bot, message, args) => {
   .setDescription(`▸ El uso correcto es: ${prefix}softwarn <@usuario> <regla infringida> (alguna nota / observación) \n▸ Usar este comando cuando se le advierta a un usuario por chat.`)
   .setFooter(`<> Obligatorio () Opcional┊Alias: ${prefix}swarn`);
 
-
   if(!args[0]) return message.channel.send(embed);
   if(!args[1]) return message.channel.send(embed);
 
@@ -60,8 +59,27 @@ module.exports.run = async (bot, message, args) => {
       "7": "Uso correcto de nicknames"
   }
 
-  let rule = reglas[args[1]] || "ERROR";
+  let rule = reglas[args[1]] || "na";
   let note = args.join(" ").slice(args[0].length + args[1].length + 2) || "na";
+
+  //errores
+  let rulesEmbed = new Discord.MessageEmbed()
+  .setColor(Colores.nocolor)
+  .setDescription(`▸ Usa el comando como \`${prefix}softwarn ${member} <N° Regla>\`.`)
+  .setFooter(`<> Obligatorio () Opcional┊Alias: ${prefix}swarn`);
+  //agregar cada regla de la variable de reglas
+  for(let i = 1; i === reglas.length; i++){
+      rulesEmbed.addField(reglas[i], `N° **${i}**`);
+  }
+
+  let alreadyWarned = new Discord.MessageEmbed()
+  .setAuthor(`| Error`, guild.iconURL())
+  .setColor(Colores.rojo)
+  .setDescription(`**—** **${member.user.tag}** ya ha sido softwarneado por infringir la regla N°${args[1]}: \`${rule}\`.
+**—** Proceder con \`${prefix}warn\`.`);
+
+
+  if(rule === "na") return message.channel.send(rulesEmbed);
 
   SoftWarn.findOne({
       userID: member.id
@@ -79,7 +97,11 @@ module.exports.run = async (bot, message, args) => {
 
             return newSoft.save();
         } else {
-            swarn.warns.push({"rule": rule, "note": note})
+            for(let i = 0; i < swarn.warns.length; i++){
+                if(swarn.warns[i].rule === rule){
+                    return message.channel.send(alreadyWarned);
+                }
+            }
         }
 
         swarn.save();
