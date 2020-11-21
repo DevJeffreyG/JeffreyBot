4d3650e12c0574852708e20c355200a0fc20cea4
JeffreyRandom <35880312+JeffreyRandom@users.noreply.github.com>
2020-11-20 22:58:14 -0500

    Update jb.js

--- jb.js
+++ jb.js
@@ -589,6 +589,7 @@ bot.on("ready", async () => {
       newInflation.save();
     } else {
       // leer y cambiar si es necesario
+      console.log("LEYENDO DURACIONES")
 
       let oldDate = new Date(dark.info.since);
       let newDate = new Date()
@@ -596,8 +597,9 @@ bot.on("ready", async () => {
       let diference1 = newDate.getTime() - oldDate.getTime();
       let pastDays = Math.floor(diference1 / (1000 * 3600 * 24));
 
+      console.log(pastDays, dark.info.date, dark.info.duration);
+
       if(pastDays >= dark.info.duration){
-        console.log(inflation, date, duration);
 
         dark.info.oldinflation = dark.info.inflation;
         dark.info.since = date;
