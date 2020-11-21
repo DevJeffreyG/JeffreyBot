1c87a1f52711ab4a3fc9dc6660d9aaac1d3a629a
JeffreyRandom <35880312+JeffreyRandom@users.noreply.github.com>
2020-11-20 23:01:18 -0500

    Update jb.js

--- jb.js
+++ jb.js
@@ -597,7 +597,7 @@ bot.on("ready", async () => {
       let diference1 = newDate.getTime() - oldDate.getTime();
       let pastDays = Math.floor(diference1 / (1000 * 3600 * 24));
 
-      console.log(pastDays, dark.info.date, dark.info.duration);
+      console.log(pastDays, dark.info.since, dark.info.duration);
 
       if(pastDays >= dark.info.duration){
 
