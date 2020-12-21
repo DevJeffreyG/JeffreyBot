f07e1b930b54654d06846ea822bdfe688c8d3c59
JeffreyRandom <35880312+JeffreyRandom@users.noreply.github.com>
2020-12-20 23:05:44 -0500

    Update serverinfo.js

--- comandos/serverinfo.js
+++ comandos/serverinfo.js
@@ -17,29 +17,8 @@ module.exports.run = async (bot, message, args) => {
     modRole = guild.roles.cache.find(x => x.id === "483105108607893533");
   }
 
-  let admins;
-  let mods;
-
-  for(let i = 0; i < adminRole.members.map(user => user).length; i++){
-    console.log(admins);
-    if(i === 0){
-      let map = adminRole.members.map(user => user);
-      admins = `${map[0].tag}`;
-    } else {
-      let map = adminRole.members.map(user => user);
-      admins = `${admins}, ${map[i].tag}`;
-    }
-  }
-
-  for(let i = 0; i < modRole.members.map(user => user).length; i++){
-    if(i === 0){
-      let map = modRole.members.map(user => user);
-      mods = `${map[0].tag}`;
-    } else {
-      let map = modRole.members.map(user => user);
-      mods = `${mods}, ${map[i].tag}`;
-    }
-  }
+  let admins = adminRole.members.map(user => user);
+  let mods = modRole.members.map(user => user);
 
   let serverembed = new Discord.MessageEmbed()
   .setTitle(`Información del server — ${message.guild.name}`)
