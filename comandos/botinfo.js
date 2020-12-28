7607c4a6d1defa0e5c7d2208a753aab6da3bd03e
JeffreyRandom <35880312+JeffreyRandom@users.noreply.github.com>
2020-12-27 23:09:15 -0500

    Emotes :100:

--- comandos/botinfo.js
+++ comandos/botinfo.js
@@ -9,27 +9,29 @@ module.exports.run = async (bot, message, args) => {
 
   if(!message.content.startsWith(prefix))return;
 
-  let added;
-  let removed;
-  let bugfix;
-  let updated;
+  let added = "<:Plus:792966881350123540>";
+  let removed = "<:Minus:792966881530609736>";
+  let updated = "<:Update:792966881690648576>";
   
   let embed = new Discord.MessageEmbed()
-  .setDescription(`**Jeffrey Bot v\`${Package.version}\` — Últimos cambios hechos al bot.**`)
+  .setDescription(`**Jeffrey Bot v\`${Package.version}\` — Últimos cambios hechos al bot.**\n\n`)
   .setColor(Colores.verde);
 
   for(let i = 0; i < changes.length; i++){
     switch(changes[i].type){
       case "added":
-        embed.addField(changes[i].title, changes[i].desc)
+        let addToDesc = `**• ${changes[i].title}**\n${added} ${changes[i].desc}.`;
+        embed.setDescription(embed.description + addToDesc)
         break;
 
       case "updated":
-        embed.addField(changes[i].title, changes[i].desc)
+        let addToDesc = `**• ${changes[i].title}**\n${updated} ${changes[i].desc}.`;
+        embed.setDescription(embed.description + addToDesc)
         break;
 
       case "removed":
-        embed.addField(changes[i].title, changes[i].desc)
+        let addToDesc = `**• ${changes[i].title}**\n${removed} ${changes[i].desc}.`;
+        embed.setDescription(embed.description + addToDesc)
         break;
     }
   }
