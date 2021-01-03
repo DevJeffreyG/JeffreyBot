e4ea0a363929980f740ef9c739fe9ec6da0e5f03
JeffreyRandom <35880312+JeffreyRandom@users.noreply.github.com>
2021-01-02 19:18:44 -0500

    collector on end darkshop testing

--- comandos/darkshop.js
+++ comandos/darkshop.js
@@ -42,8 +42,6 @@ module.exports.run = async (bot, message, args) => {
     dsChannel = guild.channels.cache.find(x => x.id === "790431676970041356");
   }
 
-  if (!message.member.roles.cache.find(x => x.id === staffRole.id)) return console.log("Un usuario ha intentado usar /darkshop: "+ author.tag);
-
   const itemPerPage = 3;
 
   // ¿es nivel 5?
@@ -165,11 +163,18 @@ Stats.findOne({
                                                         // filtros
                                                         const backwardsFilter = (reaction, user) => reaction.emoji.name === "⏪" && user.id === message.author.id;
                                                         const forwardsFilter = (reaction, user) => reaction.emoji.name === "⏩" && user.id === message.author.id;
+                                                        const collectorFilterMainPage = (reaction, user) => reaction.emoji.name === "⏩" || reaction.emoji.name === "⏪" && user.id === message.author.id;
                                 
                                                         // collectors
                                                         const backwards = msg.createReactionCollector(backwardsFilter, {time: 60000});
                                                         const forwards = msg.createReactionCollector(forwardsFilter,{time: 60000});
+                                                        const collectorMainPage = msg.createReactionCollector(collectorFilterMainPage,{time: 60000});
 
+
+                                                        collectorMainPage.on("end", r => {
+                                                            return msg.reactions.removeAll();
+                                                        })
+                                                        
                                                         // si se reacciona atrás
                                                         backwards.on("collect", r => {
                                                             if(pagn === 1) return;
