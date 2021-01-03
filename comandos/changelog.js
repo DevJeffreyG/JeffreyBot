5506f914130ee5be5991493a9d928246994b0ea3
JeffreyRandom <35880312+JeffreyRandom@users.noreply.github.com>
2021-01-02 22:28:16 -0500

    Extended details

--- comandos/changelog.js
+++ comandos/changelog.js
@@ -8,6 +8,7 @@ const changes = Config.changes;
 module.exports.run = async (bot, message, args) => {
 
   if(!message.content.startsWith(prefix))return;
+  let viewExtension = "<?>";
 
   let added = "<:Plus:792966881350123540>";
   let removed = "<:Minus:792966881530609736>";
@@ -15,13 +16,12 @@ module.exports.run = async (bot, message, args) => {
   
   let embed = new Discord.MessageEmbed()
   .setDescription(`**Jeffrey Bot v\`${Package.version}\` — Últimos cambios hechos al bot.**\n`)
+  .setFooter(`* Si estás en PC, poniendo el mouse sobre '${viewExtension}', podrás ver detalles extendidos de los cambios. — En móvil usa '${prefix}changelog extended'.`)
   .setColor(Colores.verde);
   
   let addCounter = 0;
   let removeCounter = 0;
   let updateCounter = 0;
-
-  let hasExtended = true;
 
   for(let i = 0; i < changes.length; i++){
     let addToDesc;
@@ -34,32 +34,35 @@ module.exports.run = async (bot, message, args) => {
     );
 
     switch(changes[i].type){
+    let hasExtended = true;
       case "added":
         if(!changes[i].extended) hasExtended = false;
         if(addCounter == 0){
-          addToDesc = hasExtended ? `\n**• Agregado**\n${added} ${desc} [?](${message.url} '${changes[i].extended}').\n` : `\n**• Agregado**\n${added} ${desc}.\n`;
+          addToDesc = hasExtended ? `\n**• Agregado**\n${added} ${desc}. [${viewExtension}](${message.url} '${changes[i].extended}')\n` : `\n**• Agregado**\n${added} ${desc}.\n`;
         } else {
-          addToDesc = hasExtended ? `${added} ${desc} [?](${message.url} '${changes[i].extended}').\n` : `${added} ${desc}.\n`;
+          addToDesc = hasExtended ? `${added} ${desc}. [${viewExtension}](${message.url} '${changes[i].extended}')\n` : `${added} ${desc}.\n`;
         }
         embed.setDescription(embed.description + addToDesc)
         addCounter++;
         break;
 
       case "updated":
+        if(!changes[i].extended) hasExtended = false;
         if(updateCounter == 0){
-          addToDesc = `\n**• Actualizado**\n${updated} ${desc}.\n`;
+          addToDesc = hasExtended ? `\n**• Actualizado**\n${updated} ${desc}. [${viewExtension}](${message.url} '${changes[i].extended}')\n` : `\n**• Actualizado**\n${updated} ${desc}.\n`;
         } else {
-          addToDesc = `${updated} ${desc}.\n`;
+          addToDesc = hasExtended ? `${updated} ${desc}. [${viewExtension}](${message.url} '${changes[i].extended}')\n` : `${updated} ${desc}.\n`;
         }
         embed.setDescription(embed.description + addToDesc)
         updateCounter++;
         break;
 
       case "removed":
+        if(!changes[i].extended) hasExtended = false;
         if(removeCounter == 0){
-          addToDesc = `\n**• Eliminado**\n${removed} ${desc}.\n`;
+          addToDesc = hasExtended ? `\n**• Eliminado**\n${removed} ${desc}. [${viewExtension}](${message.url} '${changes[i].extended}')\n` : `\n**• Eliminado**\n${removed} ${desc}.\n`;
         } else {
-          addToDesc = `${removed} ${desc}.\n`;
+          addToDesc = hasExtended ? `${removed} ${desc}. [${viewExtension}](${message.url} '${changes[i].extended}')\n`: `${removed} ${desc}.\n`;
         }
         embed.setDescription(embed.description + addToDesc)
         removeCounter++;
