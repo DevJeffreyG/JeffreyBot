35e695afd8fd9c57a0988205cf38e2176b6281d0
JeffreyRandom <35880312+JeffreyRandom@users.noreply.github.com>
2021-01-19 21:22:47 -0500

    Update bd.js

--- comandos/bd.js
+++ comandos/bd.js
@@ -66,9 +66,7 @@ module.exports.run = async (bot, message, args) => {
 
     userBD = query ? query : userBD;
 
-    console.log(args[0] && args[0] != "lock")
-    console.log(!args[0] || !args[1])
-    if((args[0] && args[0] != "lock") || (!args[0] || !args[1])) return message.channel.send(embed);
+    if((args[0] && args[0] != "lock") || (!args[0])) return message.channel.send(embed);
 
     switch(args[0].toLowerCase()){
       case "lock":
