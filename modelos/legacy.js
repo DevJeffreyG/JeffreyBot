14952af63ca773462589c6cae035bda96aa7642a
DevJeffreyG <gabrielpaez.r@hotmail.com>
2022-03-01 20:49:51 -0500

    Added /legacy
    The End

---
+++ modelos/legacy.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema
+
+const legacy = new Schema({
+
+  guild_id: {type: String, required: true},
+  user_list: [
+    {
+        user_id: {type: String, required: true },
+        roles: [String],
+        member_since: { type: Date }
+    }
+  ],
+  lastupdate: { type: Date, default: () => { return new Date(); }}
+})
+
+module.exports = mongoose.model("legacylist", legacy);
