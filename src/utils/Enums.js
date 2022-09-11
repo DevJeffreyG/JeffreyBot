b698d913f10f59ce9b93161c7d9e0a9fd0b066fd
JeffreyG <gabrielpaez.r@hotmail.com>
2022-09-11 17:31:28 -0500

    Update Enums.js

--- src/utils/Enums.js
+++ src/utils/Enums.js
@@ -20,10 +20,46 @@ const Categories = new Enum({
 
 const ItemTypes = new Enum({
     StackOverflow: 1,
-    ResetInterest: 2
+    ResetInterest: 2,
+    Firewall: 3,
+    Subscription: 4,
+    Temporal: 5
+})
+
+const ItemEffects = new Enum({
+    Positive: 1,
+    Negative: 2
+})
+
+const ItemActions = new Enum({
+    Add: 1,
+    Remove: 2
+})
+
+const ItemObjetives = new Enum({
+    Warns: 1,
+    Role: 2,
+    Item: 3,
+    Boost: 4
+})
+
+const BoostTypes = new Enum({
+    Multiplier: 1,
+    Probabilities: 2
+})
+
+const BoostObjetives = new Enum({
+    Jeffros: 1,
+    Exp: 2,
+    All: 3
 })
 
 module.exports = {
     Categories,
-    ItemTypes
+    ItemTypes,
+    ItemEffects,
+    ItemActions,
+    ItemObjetives,
+    BoostTypes,
+    BoostObjetives
 }
