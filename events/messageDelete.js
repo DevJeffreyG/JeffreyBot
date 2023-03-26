5d7e8ef7f2230602ecc5a3e5ac0f85c4d86ef5b4
JeffreyG <gabrielpaez.r@hotmail.com>
2023-03-26 16:24:20 -0500

    [v2.0.10] Hotfix 16

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -6,44 +6,46 @@ const { Users } = require("mongoose").models;
 module.exports = async (client, message) => {
   const author = message.author;
   const logs = await FetchAuditLogs(client, message.guild, [AuditLogEvent.MessageDelete, AuditLogEvent.MessageBulkDelete]);
+  if (logs) {
+    let info = logs[0]
 
-  let info = logs[0]
+    GenerateLog(message.guild, {
+      header: `Se ha eliminado un mensaje`,
+      footer: `Eliminado por ${info?.executor.tag ?? "un usuario"}`,
+      description: [
+        `${info?.target.tag ?? "Sin información del autor"}`,
+        `${message.content ?? message.embeds[0]?.description ?? "Sin información del mensaje"}`,
+        `ID: \`${message.id}\`.`
+      ],
+      header_icon: info?.target.displayAvatarURL({ dynamic: true }) ?? message.guild.iconURL({ dynamic: true }),
+      footer_icon: info?.executor.displayAvatarURL({ dynamic: true }),
+      color: Colores.verdejeffrey
+    })
+  }
 
-  GenerateLog(message.guild, {
-    header: `Se ha eliminado un mensaje`,
-    footer: `Eliminado por ${info?.executor.tag ?? "un usuario"}`,
-    description: [
-      `${info?.target.tag ?? "Sin información del autor"}`,
-      `${message.content ?? message.embeds[0]?.description ?? "Sin información del mensaje"}`,
-      `ID: \`${message.id}\`.`
-    ],
-    header_icon: info?.target.displayAvatarURL({ dynamic: true }) ?? message.guild.iconURL({ dynamic: true }),
-    footer_icon: info?.executor.displayAvatarURL({ dynamic: true }),
-    color: Colores.verdejeffrey
-  })
+  if (author) {
 
-  if (!author) return
+    let user = await Users.getOrCreate({ user_id: author.id, guild_id: message.guild.id });
 
-  let user = await Users.getOrCreate({ user_id: author.id, guild_id: message.guild.id });
+    if (user.data.cooldowns.chat_rewards) {
+      if (message.channel.id != Bases.owner.channels.mainChannel) return; // TODO: arreglar esto ############################################
 
-  if (user.data.cooldowns.chat_rewards) {
-    if (message.channel.id != Bases.owner.channels.mainChannel) return; // TODO: arreglar esto ############################################
+      let global = user.economy.global
 
-    let global = user.economy.global
+      let nxtLvl = 10 * ((global.level - 1) ** 2) + 50 * (global.level - 1) + 100; // fórmula de MEE6.
 
-    let nxtLvl = 10 * ((global.level - 1) ** 2) + 50 * (global.level - 1) + 100; // fórmula de MEE6.
-
-    global.currency -= user.data.lastGained.currency;
+      global.currency -= user.data.lastGained.currency;
 
 
-    if (global.exp - user.data.lastGained.exp >= nxtLvl) console.log("Subió de nivel");
-    else {
-      global.exp -= user.data.lastGained.exp;
+      if (global.exp - user.data.lastGained.exp >= nxtLvl) console.log("Subió de nivel");
+      else {
+        global.exp -= user.data.lastGained.exp;
+      }
+
+      await user.save();
+
+      console.log(global.currency, global.exp, author.username);
     }
-
-    await user.save();
-
-    console.log(global.currency, global.exp, author.username);
   }
 
   let filteredCollectors = client.activeCollectors.filter(x => {
