665c9e8f0d7901b398dd8a79cd082453b8759909
Gabriel Páez <gabrielpaez.r@hotmail.com>
2023-06-07 12:52:17 -0500

    Se disolvió /admin

---
+++ commands/staff/setup/shop.js
@@ -0,0 +1,213 @@
+const { SlashCommandIntegerOption } = require("discord.js");
+const { Command, Shop, Confirmation, ShopTypes, Enum, ItemActions, ItemObjetives, ItemEffects, ItemTypes, BoostTypes, BoostObjetives } = require("../../../src/utils");
+
+const command = new Command({
+    name: "admin-shop",
+    desc: "Administración de las tiendas"
+})
+
+command.data
+    .addSubcommand(itemlist =>
+        itemlist
+            .setName("item-list")
+            .setDescription("Lista de todos los items de la tienda, los que tienen usos y los que no.")
+    )
+    .addSubcommand(adddiscount =>
+        adddiscount
+            .setName("add-discount")
+            .setDescription("Agrega un nuevo descuento para usuarios por nivel")
+            .addIntegerOption(o =>
+                o
+                    .setName("nivel")
+                    .setDescription("El nivel al cual se aplicará el descuento")
+                    .setRequired(true)
+            )
+            .addIntegerOption(o =>
+                o
+                    .setName("descuento")
+                    .setDescription("El descuento aplicado (en porcentaje, ej: 10, 15, 50) [0 para eliminar]")
+                    .setRequired(true)
+            )
+    )
+    .addSubcommand(additem =>
+        additem
+            .setName("add-item")
+            .setDescription("Agregar un item a alguna de las tiendas")
+            .addStringOption(o =>
+                o.setName("nombre")
+                    .setDescription("El nombre del item")
+                    .setRequired(true))
+            .addStringOption(o =>
+                o.setName("descripcion")
+                    .setDescription("La descripción del item")
+                    .setRequired(true))
+            .addIntegerOption(o =>
+                o.setName("precio")
+                    .setDescription("El precio base del item")
+                    .setRequired(true))
+    )
+    .addSubcommand(delitem =>
+        delitem
+            .setName("del-item")
+            .setDescription("Eliminar un item de alguna de las tiendas")
+            .addIntegerOption(o =>
+                o.setName("id")
+                    .setDescription("La ID del item para eliminar")
+                    .setRequired(true)
+            )
+    )
+    .addSubcommand(useinfo =>
+        useinfo
+            .setName("use-info")
+            .setDescription("Editar el uso que tiene un item de la tienda. [DS] = DarkShop")
+            .addIntegerOption(option =>
+                option
+                    .setName("id")
+                    .setDescription("La id del item a editar")
+                    .setRequired(true)
+            )
+            .addIntegerOption(option =>
+                option
+                    .setName("accion")
+                    .setDescription("¿Se agrega o elimina el 'objetivo'?")
+                    .addChoices(
+                        { name: "Agrega", value: ItemActions.Add },
+                        { name: "Elimina", value: ItemActions.Remove }
+                    )
+                    .setRequired(true)
+            )
+            .addIntegerOption(option =>
+                option
+                    .setName("objetivo")
+                    .setDescription("El objetivo al usar el item: ¿a qué se hará la accion?")
+                    .addChoices(...new Enum(ItemObjetives).complexArray())
+                    .setRequired(true)
+            )
+            .addRoleOption(option =>
+                option
+                    .setName("role")
+                    .setDescription("¿Qué role se agrega/elimina?")
+            )
+            .addIntegerOption(option =>
+                option
+                    .setName("boostobj")
+                    .setDescription("El objetivo del boost (si es un boost)")
+                    .addChoices(
+                        { name: "Dinero", value: BoostObjetives.Currency },
+                        { name: "EXP", value: BoostObjetives.Exp },
+                        { name: "Todo", value: BoostObjetives.All }
+                    )
+            )
+            .addIntegerOption(option =>
+                option
+                    .setName("boosttype")
+                    .setDescription("El tipo del boost (si es un boost)")
+                    .addChoices(...new Enum(BoostTypes).complexArray())
+            )
+            .addNumberOption(option => option
+                .setName("boostval")
+                .setDescription("Valor del boost (si es un boost)")
+                .setMinValue(0.01)
+            )
+            .addIntegerOption(option =>
+                option
+                    .setName("cantidad")
+                    .setDescription("¿Cuántos 'items' se agregarán al inventario?")
+            )
+            .addStringOption(option =>
+                option
+                    .setName("reply")
+                    .setDescription("Mensaje que se envía después de usar el item. No llenar esto para dejar el actual")
+            )
+            .addStringOption(option =>
+                option
+                    .setName("duracion")
+                    .setDescription("Si es un tipo role o boost, ¿cuánto dura? 1d, 7d, 10m, etc")
+            )
+            .addIntegerOption(o =>
+                o
+                    .setName("especial")
+                    .setDescription("Si es algún item especial, el tipo que es.")
+                    .addChoices(
+                        { name: "[DS] Firewall", value: ItemTypes.Firewall },
+                        { name: "[DS] ~(+5%) Precisión (Stack Overflow)", value: ItemTypes.StackOverflow },
+                        { name: "[DS] Reset Interest", value: ItemTypes.ResetInterest },
+                        { name: "[DS] Skip Firewall", value: ItemTypes.SkipFirewall }
+                    )
+            )
+            .addIntegerOption(option =>
+                option
+                    .setName("efecto")
+                    .setDescription("Si el efecto es positivo o negativo al usarse [DS]")
+                    .addChoices(...new Enum(ItemEffects).complexArray())
+            )
+    )
+    .addSubcommand(toggle =>
+        toggle
+            .setName("toggle")
+            .setDescription("Ocultar un item de la tienda")
+            .addIntegerOption(option =>
+                option.setName("id")
+                    .setDescription("La id del item a alternar")
+                    .setRequired(true))
+            .addStringOption(option =>
+                option.setName("duracion")
+                    .setDescription("¿Cuánto tiempo estará oculto? - 1d, 1h, 10m, 30s, etc."))
+    )
+    .addSubcommand(edit =>
+        edit
+            .setName("edit")
+            .setDescription("Edita un item creado")
+    )
+
+command.addOptionsTo(["item-list", "add-discount", "add-item", "del-item", "use-info", "toggle", "edit"], [
+    new SlashCommandIntegerOption()
+        .setName("tipo")
+        .setDescription("El tipo de tienda de este item")
+        .setChoices(...new Enum(ShopTypes).complexArray())
+        .setRequired(true)
+])
+
+command.execute = async (interaction, models, params, client) => {
+    await interaction.deferReply();
+
+    const { Shops, DarkShops } = models;
+    const { subcommand, shop } = params;
+    const { nivel, descuento, darkshop, id } = params[subcommand];
+
+    const isDarkShop = darkshop?.value;
+
+    const doc = isDarkShop ? await DarkShops.getWork(interaction.guild.id) : await Shops.getWork(interaction.guild.id);
+    const _shop = new Shop(doc, interaction, isDarkShop);
+
+    switch (subcommand) {
+        case "item-list":
+            return _shop.showAllItems();
+
+        case "add-discount":
+            return _shop.addDiscount(nivel.value, descuento.value);
+
+        case "add-item":
+            return _shop.addItem(shop);
+
+        case "del-item":
+            let confirmation = await Confirmation("Eliminar item", [
+                `El item con Id \`${id.value}\` de la ${isDarkShop ? "DarkShop" : "tienda"}.`,
+                `Se eliminará el item de todos los inventarios.`,
+                `No se devolverá dinero.`
+            ], interaction)
+
+            if (!confirmation) return;
+            return _shop.removeItem(id.value);
+        case "use-info":
+            return await _shop.editUse(items)
+
+        case "toggle":
+            return await _shop.toggleItem(id.value, duracion?.value);
+
+        case "edit":
+            return await _shop.editItem(items, subcommand)
+    }
+}
+
+module.exports = command;
